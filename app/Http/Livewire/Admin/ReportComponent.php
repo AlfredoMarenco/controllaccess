3d15c2595332d69b7f9411d1f0901af354c17a99
Alfredo Gonzalez Marenco <alfredomarenco@boletea.com>
2024-11-08 20:36:30 -0600

    Reportes mejorados

--- app/Http/Livewire/Admin/ReportComponent.php
+++ app/Http/Livewire/Admin/ReportComponent.php
@@ -7,9 +7,25 @@ use Livewire\Component;
 
 class ReportComponent extends Component
 {
+    public $principal = true;
+    public $interior = false;
+
+    public function showPrincipal()
+    {
+        $this->principal = true;
+        $this->interior = false;
+    }
+
+    public function showInterior()
+    {
+        $this->principal = false;
+        $this->interior = true;
+    }
     public function render()
     {
-        return view('livewire.admin.report-component',[
+
+
+        return view('livewire.admin.report-component', [
             'boxs' => Box::all(),
         ]);
     }
--- resources/views/livewire/admin/report-component.blade.php
+++ resources/views/livewire/admin/report-component.blade.php
@@ -8,56 +8,120 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="px-4 pb-4 mb-4 shadow-xl bg-white">
-                    <div class="grid lg:grid-cols-5 lg:gap-8 grid-cols-1 gap-2 place-content-center p-4">
-                        @foreach ($boxs as $palco)
-                            <div>
-                                <h1 class="text-center font-bold p-2">PALCO {{ $palco->name }} {{ $palco->identifier }}</h1>
-                                <div class="grid grid-cols-4 gap-2 place-content-center">
-                                    @foreach ($palco->codes as $seat)
-                                        @if ($seat->box_id == $palco->id )
-                                            @if ($seat->status == 1)
-                                                <div
-                                                    class="flex justify-center items-center bg-green-600 text-white text-lg font-bold shadow-lg">
-                                                    <p>{{ $seat->seat }}</p>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="w-4 h-4">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                                                    </svg>
-
-                                                </div>
-                                            @elseif($seat->status == 2)
-                                                <div
-                                                    class="flex justify-center items-center bg-orange-600 text-white text-lg font-bold shadow-lg">
-                                                    <p>{{ $seat->seat }}</p>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="w-4 h-4">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
-                                                    </svg>
-                                                </div>
-                                            @else
-                                                <div
-                                                    class="flex bg-red-600 justify-center text-white text-lg font-bold shadow-lg items-center">
-                                                    <p>{{ $seat->seat }}</p>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                        class="w-4 h-4">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
-                                                    </svg>
-                                                </div>
-                                            @endif
-                                        @endif
-                                    @endforeach
-                                </div>
-                            </div>
-                        @endforeach
-                    </div>
+                <div class="underline-offset-1 mx-auto font-semibold p-4 cursor-pointer hover:text-blue-700">
+                    @if ($principal)
+                        <h1 wire:click="showInterior()">Ver acceso interior</h1>
+                    @else
+                        <h1 wire:click="showPrincipal()">Ver acceso principal</h1>
+                    @endif
                 </div>
+                @if ($principal)
+                <h1 class=" text-center font-semibold text-3xl text-red-600">Filtro Principal</h1>
+                    <div class="px-4 pb-4 mb-4 shadow-xl bg-white">
+                        <div class="grid lg:grid-cols-5 lg:gap-8 grid-cols-1 gap-2 place-content-center p-4">
+                            @foreach ($boxs as $palco)
+                                <div>
+                                    <h1 class="text-center font-bold p-2">PALCO {{ $palco->name }}
+                                        {{ $palco->identifier }}</h1>
+                                    <div class="grid grid-cols-4 gap-2 place-content-center">
+                                        @foreach ($palco->codes as $seat)
+                                            @if ($seat->box_id == $palco->id)
+                                                @if ($seat->status == 1)
+                                                    <div
+                                                        class="flex justify-center items-center bg-green-600 text-white text-lg font-bold shadow-lg">
+                                                        <p>{{ $seat->seat }}</p>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-4 h-4">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                                        </svg>
+
+                                                    </div>
+                                                @elseif($seat->status == 2)
+                                                    <div
+                                                        class="flex justify-center items-center bg-orange-600 text-white text-lg font-bold shadow-lg">
+                                                        <p>{{ $seat->seat }}</p>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-4 h-4">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                                                        </svg>
+                                                    </div>
+                                                @else
+                                                    <div
+                                                        class="flex bg-red-600 justify-center text-white text-lg font-bold shadow-lg items-center">
+                                                        <p>{{ $seat->seat }}</p>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-4 h-4">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                                        </svg>
+                                                    </div>
+                                                @endif
+                                            @endif
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @else
+                <h1 class=" text-center font-semibold text-3xl text-blue-600">Filtro Interior</h1>
+                    <div class="px-4 pb-4 mb-4 shadow-xl bg-white">
+                        <div class="grid lg:grid-cols-5 lg:gap-8 grid-cols-1 gap-2 place-content-center p-4">
+                            @foreach ($boxs as $palco)
+                                <div>
+                                    <h1 class="text-center font-bold p-2">PALCO {{ $palco->name }}
+                                        {{ $palco->identifier }}</h1>
+                                    <div class="grid grid-cols-4 gap-2 place-content-center">
+                                        @foreach ($palco->codes as $seat)
+                                            @if ($seat->box_id == $palco->id)
+                                                @if ($seat->status2 == 1)
+                                                    <div
+                                                        class="flex justify-center items-center bg-green-600 text-white text-lg font-bold shadow-lg">
+                                                        <p>{{ $seat->seat }}</p>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-4 h-4">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                                                        </svg>
+
+                                                    </div>
+                                                @elseif($seat->status2 == 2)
+                                                    <div
+                                                        class="flex justify-center items-center bg-orange-600 text-white text-lg font-bold shadow-lg">
+                                                        <p>{{ $seat->seat }}</p>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-4 h-4">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9" />
+                                                        </svg>
+                                                    </div>
+                                                @else
+                                                    <div
+                                                        class="flex bg-red-600 justify-center text-white text-lg font-bold shadow-lg items-center">
+                                                        <p>{{ $seat->seat }}</p>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                            class="w-4 h-4">
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                                                        </svg>
+                                                    </div>
+                                                @endif
+                                            @endif
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </div>
